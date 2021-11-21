ee3c4d51a109c2e2e077698c8a66028d7f354fbe
RodneyHill <rlhill888@gmail.com>
2021-11-20 23:53:04 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(){
+    (1+80);
+}
+function subtract(){
+    60 - 40;
+}
+function multiply(){
+    8*8;
+}
+function divide(){
+    27/3;
+}
+function add (a, b){
+    return a+b;
+}
+function subtract(a, b){
+    return a-b;
+}
+function multiply(a, b){
+    return a*b;
+}
+function divide(a, b){
+    return a/b;
+}
+function increment(n){
+    return n + 1;
+}
+function decrement(n){
+    return n - 1;
+}
+function makeInt(n){
+return parseInt(n, 10);
+}
+function preserveDecimal(n){
+return parseFloat(n);
+}
+
+
+
+
